5b94b206777dd359c7664d22317ab794f70d7376
pollockcl <pollock.clee@gmail.com>
2018-06-26 14:49:59 -0600

    removed whitespace

--- spec/requests/foods_index_spec.rb
+++ spec/requests/foods_index_spec.rb
@@ -7,8 +7,8 @@ describe 'GET to api/v1/foods' do
       create_list(:food, 3)
       get '/api/v1/foods'
 
-      body   = JSON.parse(response.body)
-      keys   = %w[id name calories]
+      body = JSON.parse(response.body)
+      keys = %w[id name calories]
 
       body[0].keys.each do |key|
         expect(key).to be_in(keys)
