54f4355ba987c8c5187d1710ed0a6bac5e51234e
pollockcl <pollock.clee@gmail.com>
2018-06-27 14:07:50 -0600

    the food object is returned upon successfull creation

--- spec/requests/foods_new_create_spec.rb
+++ spec/requests/foods_new_create_spec.rb
@@ -10,5 +10,14 @@ describe 'POST /api/v1/foods' do
       expect(food.name).to eq("Cake")
       expect(food.calories).to eq(100)
     end
+
+    it 'renders the json of the food' do
+      post '/api/v1/foods?name=Cake&calories=100', params: { food: { name: "Cake", calories: 100 } }
+
+      body = JSON.parse(response.body)
+
+      expect(body['name']).to eq("Cake")
+      expect(body['calories']).to eq(100)
+    end
   end
 end
